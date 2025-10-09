6040d3e00663bced0e575cb78bcc5b7c058a11d6
Noudvw <noudvanwiggen@gmail.com>
2025-10-09 17:15:02 +0200

    Represent a table with contribution for each player. The set-up should be compatible with both 3-player and 4-player groups

--- PGG/__init__.py
+++ PGG/__init__.py
@@ -31,13 +31,23 @@ class Group(BaseGroup):
 
     def compute_group_earnings(self):
         self.collective_contribution = sum(p.contribution for p in self.get_players())
-        self.PG_earnings = self.collective_contribution * C.MPCR
-
+        unrounded_earnings = self.collective_contribution * C.MPCR
+        self.PG_earnings = round(unrounded_earnings, 2)
 
     def compute_earnings(self):
         for p in self.get_players():
             p.remaining_endowment = p.endowment - p.contribution
             p.earnings = p.remaining_endowment + self.PG_earnings
+
+    def set_other_contributions(self):
+        for p in self.get_players():
+                others = p.get_others_in_group()
+                if len(others) > 0:
+                    p.p2_contribution = others[0].contribution
+                if len(others) > 1:
+                    p.p3_contribution = others[1].contribution
+                if len(others) > 2:
+                    p.p4_contribution = others[2].contribution
 
 
 class Player(BasePlayer):
@@ -45,9 +55,18 @@ class Player(BasePlayer):
     remaining_endowment = models.IntegerField()
     contribution = models.IntegerField()
     earnings = models.FloatField()
+    p2_contribution = models.IntegerField()
+    p3_contribution = models.IntegerField()
+    p4_contribution = models.IntegerField()
 
     def setup_round(self):
         self.endowment = C.ENDOWMENT
+
+    def coplayer0(self):
+        return self.get_others_in_group()[0]
+
+    def coplayer1(self):
+        return self.get_others_in_group()[1]
 
 class SetUpRound(WaitPage):
     wait_for_all_groups = True
@@ -74,6 +93,7 @@ class ComputeResults(WaitPage):
     def after_all_players_arrive(group):
         group.compute_group_earnings()
         group.compute_earnings()
+        group.set_other_contributions()
 
 class Results(Page):
     pass
