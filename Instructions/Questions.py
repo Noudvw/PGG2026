3eca427a0ed588ff8a0cedaa311980bd568103fa
Noudvw <noudvanwiggen@gmail.com>
2026-02-27 10:55:06 +0100

    Added questions to the instructions part. Current plan: split the instructions into three pages, with three instruction sets and three question sets
    Next:
    - Watch for computation errors
    - Test treatments and 4-player setting
    - Make game attrition-robust
    - Add all stages of the game

--- Instructions/Questions.py
+++ Instructions/Questions.py
@@ -3,14 +3,35 @@
 question_data = {
     1: {
         "question_answer": 20,
-        "question_hint": (
-            "Your answer to question 1 is incorrect. Reminder: Earnings consist of remaining endowment and earnings received from the group project"
-        ),
+        "hint_group":"A",
     },
     2: {
         "question_answer": 20,
-        "question_hint": (
-            "Your answer to question 2 is incorrect. Reminder:Earnings consist of remaining endowment and earnings received from the group project"
-        ),
+        "hint_group":"A"
     },
+    3: {
+        "question_answer": 32,
+        "hint_group":"B",
+    },
+    4: {
+        "question_answer": 32,
+        "hint_group":"B"
+    },
+    5: {
+        "question_answer": 36,
+        "hint_group":"C",
+    },
+    6: {
+        "question_answer": 30,
+        "hint_group":"C"
+    },
+}
+
+hint_messages = {
+    "A": "Earnings consist of two parts: remaining endowment and earnings received from the group project. "
+         "For these questions, earnings from the group project are zero",
+    "B": "Earnings consist of two parts: remaining endowment and earnings received from the group project."
+          "For these questions, the remaining endowment is zero",
+    "C": "Earnings consist of two parts: remaining endowment and earnings received from the group project. "
+         "To solve this question, you need to calculate remaining endowment and earnings from the group project, and then add both"
 }
--- Instructions/__init__.py
+++ Instructions/__init__.py
@@ -35,25 +35,53 @@ class Player(BasePlayer):
     attempt_count = models.IntegerField(initial=0)
     question1 = models.IntegerField(label ="Your income?" )
     question2 = models.IntegerField(label ="The income of the other group members?" )
-
+    question3 = models.IntegerField(label="Your income?")
+    question4 = models.IntegerField(label="The income of the other group members?")
+    question5 = models.IntegerField(label="Your income if you contribute 0 Points to the group project?")
+    question6 = models.IntegerField(label="Your income if you contribute 10 Points to the group project?")
 
 # PAGES
 class QuestionsPGG(Page):
     form_model = "player"
-    form_fields = ["question1", "question2"]
+    form_fields = ["question1", "question2", "question3", "question4", "question5", "question6"]
 
+
+    # This error message method checks whether answers are correct, and only allows players to the next page if they are.
+    # In case players make mistakes, the attempt_count is increased by one and players can try again.
+    # Players receive a message about the questions they answered incorrectly and a hint corresponding to that question.
     @staticmethod
     def error_message(player: Player, values: dict) -> str | None:
         player.attempt_count += 1
-        if values["question1"] != Questions.question_data[1]["question_answer"]:
-            return Questions.question_data[1]["question_hint"]
-        if values["question2"] != Questions.question_data[2]["question_answer"]:
-            return Questions.question_data[2]["question_hint"]
-        return None
+        incorrect_questions = []
+        hint_to_questions = {}
+
+        # This part of code checks which answers are incorrect
+        for qnum, qdata in Questions.question_data.items():
+            if values[f"question{qnum}"] != qdata["question_answer"]:
+                incorrect_questions.append(qnum)
+                group = qdata["hint_group"]
+                hint_to_questions.setdefault(group, []).append(qnum)
+        if not incorrect_questions:
+                return None
+        messages = []
+
+        #The following line shows the players which questions they answered incorrectly
+        messages.append(
+            f"Your answers to the following questions are incorrect: "
+            f"{', '.join(map(str, incorrect_questions))}"
+        )
+
+        #The following line is used for displaying hints, depending on which questions were answered incorrect
+        for group, qnums in hint_to_questions.items():
+            qlist = ",".join(map(str, qnums))
+            messages.append(
+                f"Hint {group} (for question{'s' if len(qnums) > 1 else ''} {qlist}): "
+                f"{Questions.hint_messages[group]}"
+            )
+        return "<br><br>".join(messages)
 
 
 class WaitPageCompleted(WaitPage):
-    template_name = "quiz/WaitPageCompleted.html"
     wait_for_all_groups = True
 
     @staticmethod
