e54e090538a5046fde13cfe1c407a18e283bc669
Noudvw <noudvanwiggen@gmail.com>
2026-02-27 11:35:46 +0100

    Added comprehension questions in three pages: for the PGG, for the deduct points part, and for the bonus earnings. Bonus earnings questions can use some refinement. Mistakes are logged (per question), the page also notifies participants with which question was answered incorrectly and gives a hint in case participants make mistakes. I consider kicking people out in case they make too many mistakes, but haven't coded this part in yet. Next step: add the actual instructions text in the HTML.
    Next:
    - Instructions: add the actual instructions (explanations)
    - Watch for computation errors
    - Test treatments and 4-player setting
    - Make game attrition-robust
    - Add all stages of the game

--- Instructions/Questions.py
+++ Instructions/Questions.py
@@ -4,26 +4,52 @@ question_data = {
     1: {
         "question_answer": 20,
         "hint_group":"A",
+        "page": 1
     },
     2: {
         "question_answer": 20,
-        "hint_group":"A"
+        "hint_group":"A",
+        "page": 1
     },
     3: {
         "question_answer": 32,
         "hint_group":"B",
+        "page": 1
     },
     4: {
         "question_answer": 32,
-        "hint_group":"B"
+        "hint_group":"B",
+        "page": 1
     },
     5: {
         "question_answer": 36,
         "hint_group":"C",
+        "page": 1
     },
     6: {
         "question_answer": 30,
-        "hint_group":"C"
+        "hint_group":"C",
+        "page": 1
+    },
+    7: {
+        "question_answer": 5,
+        "hint_group":"D",
+        "page": 2
+    },
+    8: {
+        "question_answer": 0,
+        "hint_group":"D",
+        "page": 2
+    },
+    9: {
+        "question_answer": 21,
+        "hint_group":"D",
+        "page": 2
+    },
+    10: {
+        "question_answer": 4,
+        "hint_group":"E",
+        "page": 3
     },
 }
 
@@ -33,5 +59,7 @@ hint_messages = {
     "B": "Earnings consist of two parts: remaining endowment and earnings received from the group project."
           "For these questions, the remaining endowment is zero",
     "C": "Earnings consist of two parts: remaining endowment and earnings received from the group project. "
-         "To solve this question, you need to calculate remaining endowment and earnings from the group project, and then add both"
+         "To solve this question, you need to calculate remaining endowment and earnings from the group project, and then add both",
+    "D": "Assigning Deduction Points costs you 1 Point and costs the receiver 3 Points",
+    "E": "The bonus for answering a question correct is 4 Points, as long as it is the question_that_counts"
 }
--- Instructions/__init__.py
+++ Instructions/__init__.py
@@ -33,17 +33,33 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     attempt_count = models.IntegerField(initial=0)
-    question1 = models.IntegerField(label ="Your income?" )
-    question2 = models.IntegerField(label ="The income of the other group members?" )
-    question3 = models.IntegerField(label="Your income?")
-    question4 = models.IntegerField(label="The income of the other group members?")
-    question5 = models.IntegerField(label="Your income if you contribute 0 Points to the group project?")
-    question6 = models.IntegerField(label="Your income if you contribute 10 Points to the group project?")
+    question1 = models.IntegerField(label ="Q1: Your income?" )
+    question2 = models.IntegerField(label ="Q2: The income of the other group members?" )
+    question3 = models.IntegerField(label="Q3: Your income?")
+    question4 = models.IntegerField(label="Q4: The income of the other group members?")
+    question5 = models.IntegerField(label="Q5: Your income if you contribute 0 Points to the group project?")
+    question6 = models.IntegerField(label="Q6: Your income if you contribute 10 Points to the group project?")
+    question7 = models.IntegerField(label="Q7: By how many Points will your income be reduced by assigning Deduction Points?")
+    question8 = models.IntegerField(label="Q8: By how many Points will your income be reduced by assigning Deduction Points?")
+    question9 = models.IntegerField(label="Q9: By how many Points will your income be reduced by receiving Deduction Points?")
+    question10 = models.IntegerField(label="Q10: How many Points will be added to your income?")
 
+
+    q1mistakes = models.IntegerField(initial=0)
+    q2mistakes = models.IntegerField(initial=0)
+    q3mistakes = models.IntegerField(initial=0)
+    q4mistakes = models.IntegerField(initial=0)
+    q5mistakes = models.IntegerField(initial=0)
+    q6mistakes = models.IntegerField(initial=0)
+    q7mistakes = models.IntegerField(initial=0)
+    q8mistakes = models.IntegerField(initial=0)
+    q9mistakes = models.IntegerField(initial=0)
+    q10mistakes = models.IntegerField(initial=0)
 # PAGES
 class QuestionsPGG(Page):
     form_model = "player"
     form_fields = ["question1", "question2", "question3", "question4", "question5", "question6"]
+    Qpage = 1
 
 
     # This error message method checks whether answers are correct, and only allows players to the next page if they are.
@@ -57,8 +73,101 @@ class QuestionsPGG(Page):
 
         # This part of code checks which answers are incorrect
         for qnum, qdata in Questions.question_data.items():
+            if qdata["page"] != QuestionsPGG.Qpage:
+                continue
+
             if values[f"question{qnum}"] != qdata["question_answer"]:
                 incorrect_questions.append(qnum)
+                field = f"q{qnum}mistakes"
+                setattr(player, field, getattr(player, field) + 1)
+                group = qdata["hint_group"]
+                hint_to_questions.setdefault(group, []).append(qnum)
+        if not incorrect_questions:
+                return None
+        messages = []
+
+        #The following line shows the players which questions they answered incorrectly
+        messages.append(
+            f"Your answers to the following questions are incorrect: "
+            f"{', '.join(map(str, incorrect_questions))}"
+        )
+
+        #The following line is used for displaying hints, depending on which questions were answered incorrect
+        for group, qnums in hint_to_questions.items():
+            qlist = ",".join(map(str, qnums))
+            messages.append(
+                f"Hint {group} (for question{'s' if len(qnums) > 1 else ''} {qlist}): "
+                f"{Questions.hint_messages[group]}"
+            )
+        return "<br><br>".join(messages)
+
+class QuestionsDP(Page):
+    form_model = "player"
+    form_fields = ["question7", "question8", "question9"]
+    Qpage = 2
+
+
+    # This error message method checks whether answers are correct, and only allows players to the next page if they are.
+    # In case players make mistakes, the attempt_count is increased by one and players can try again.
+    # Players receive a message about the questions they answered incorrectly and a hint corresponding to that question.
+    @staticmethod
+    def error_message(player: Player, values: dict) -> str | None:
+        player.attempt_count += 1
+        incorrect_questions = []
+        hint_to_questions = {}
+
+        # This part of code checks which answers are incorrect
+        for qnum, qdata in Questions.question_data.items():
+            if qdata["page"] != QuestionsDP.Qpage:
+                continue
+            if values[f"question{qnum}"] != qdata["question_answer"]:
+                incorrect_questions.append(qnum)
+                field = f"q{qnum}mistakes"
+                setattr(player, field, getattr(player, field) + 1)
+                group = qdata["hint_group"]
+                hint_to_questions.setdefault(group, []).append(qnum)
+        if not incorrect_questions:
+                return None
+        messages = []
+
+        #The following line shows the players which questions they answered incorrectly
+        messages.append(
+            f"Your answers to the following questions are incorrect: "
+            f"{', '.join(map(str, incorrect_questions))}"
+        )
+
+        #The following line is used for displaying hints, depending on which questions were answered incorrect
+        for group, qnums in hint_to_questions.items():
+            qlist = ",".join(map(str, qnums))
+            messages.append(
+                f"Hint {group} (for question{'s' if len(qnums) > 1 else ''} {qlist}): "
+                f"{Questions.hint_messages[group]}"
+            )
+        return "<br><br>".join(messages)
+
+
+class QuestionsBonus(Page):
+    form_model = "player"
+    form_fields = ["question10"]
+    Qpage = 3
+
+    # This error message method checks whether answers are correct, and only allows players to the next page if they are.
+    # In case players make mistakes, the attempt_count is increased by one and players can try again.
+    # Players receive a message about the questions they answered incorrectly and a hint corresponding to that question.
+    @staticmethod
+    def error_message(player: Player, values: dict) -> str | None:
+        player.attempt_count += 1
+        incorrect_questions = []
+        hint_to_questions = {}
+
+        # This part of code checks which answers are incorrect
+        for qnum, qdata in Questions.question_data.items():
+            if qdata["page"] != QuestionsBonus.Qpage:
+                continue
+            if values[f"question{qnum}"] != qdata["question_answer"]:
+                incorrect_questions.append(qnum)
+                field = f"q{qnum}mistakes"
+                setattr(player, field, getattr(player, field) + 1)
                 group = qdata["hint_group"]
                 hint_to_questions.setdefault(group, []).append(qnum)
         if not incorrect_questions:
@@ -91,5 +200,7 @@ class WaitPageCompleted(WaitPage):
 
 page_sequence = [
     QuestionsPGG,
+    QuestionsDP,
+    QuestionsBonus,
     WaitPageCompleted,
 ]
